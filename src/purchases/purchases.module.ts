f175803e1b8cd5f1a1e70e25d3d217d4498db5e8
incora-dev <dev@incorainc.com>
2020-12-09 18:37:04 +0200

    hotfix

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/products/product.entity';
-import { Users } from 'src/users/user.entity';
 import { Purchase } from './entities/purchase.entity';
 import { PurchaseItem } from './entities/purchaseItem.entity';
 import { PurchasesController } from './purchases.controller';
