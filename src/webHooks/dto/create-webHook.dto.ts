cb74a56bf5956784402f389ba481ee38716409bc
incora-dev <dev@incorainc.com>
2020-12-15 19:02:12 +0200

    fix

--- src/webHooks/dto/create-webHook.dto.ts
+++ src/webHooks/dto/create-webHook.dto.ts
@@ -2,10 +2,7 @@ import { ApiProperty } from '@nestjs/swagger/dist/decorators';
 import { IsUrl } from 'class-validator';
 
 export class CreateWebHookDto {
-  @ApiProperty({
-    minLength: 2,
-    maxLength: 25,
-  })
+  @ApiProperty()
   @IsUrl()
   url: string;
 }
